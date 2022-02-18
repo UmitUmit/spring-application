fc3568371affe3c7447e82550c5ea16293f4adae
UmitUmit <umit.green@gmail.com>
2022-02-17 20:40:53 -0500

    Wiring

---
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/DealerApp.java
@@ -0,0 +1,24 @@
+package com.cydeo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DealerApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigCar.class);
+
+        Car c = container.getBean(Car.class);
+        Person p = container.getBean(Person.class);
+
+
+        System.out.println("Person's nanme" + p.getName());
+        System.out.println("Car's name : " + c.getMake());
+        System.out.println("Person's car " + p.getCar().getMake());
+
+    }
+
+
+
+
+}
