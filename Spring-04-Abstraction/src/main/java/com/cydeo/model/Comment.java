e0fd27a3c318013eed687d8a0ec1df3a7e720622
UmitUmit <umit.green@gmail.com>
2022-02-24 18:50:21 -0500

    Abstraction

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/model/Comment.java
@@ -0,0 +1,13 @@
+package com.cydeo.model;
+
+
+import com.cydeo.proxy.CommentNotificationProxy;
+import lombok.Data;
+
+@Data
+public class Comment {
+    private String author;
+    private String text;
+}
+
+
