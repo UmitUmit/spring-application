76bd7acbd98b73759f2b2aada89fffb64b8fe721
UmitUmit <umit.green@gmail.com>
2022-02-24 19:05:25 -0500

    BeanScope practice

---
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/repository/DBCommentRepository.java
@@ -0,0 +1,16 @@
+package com.cydeo.repository;
+
+import com.cydeo.model.Comment;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class DBCommentRepository implements CommentRepository{
+    @Override
+    public void storeComment(Comment comment) {
+        System.out.println("Storing comment: " + comment.getText());
+    }
+}
