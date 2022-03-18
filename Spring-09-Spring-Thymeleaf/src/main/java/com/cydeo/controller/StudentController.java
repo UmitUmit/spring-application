98607e895f8f6d086eb040db21ef0bbadabd7c8e
UmitUmit <umit.green@gmail.com>
2022-03-17 20:40:13 -0400

    Spring thymeleaf --

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,13 +3,18 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+    //@GetMapping and @PostMapping
+
+    //@RequestMapping(value = "/register"  , method = RequestMethod.GET)
+    @GetMapping(value = "/register" )
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
