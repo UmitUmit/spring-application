851f7e9716d3fe11c893b323109e1011711771b3
UmitUmit <umit.green@gmail.com>
2022-04-19 19:29:29 -0400

    Task

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -32,11 +32,11 @@ public class DataGenerator implements CommandLineRunner {
         List<Department> departmentList = new ArrayList<>();
 
 
-        Employee e1 = new Employee("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006,04,20), Gender.F,154864);
-        Employee e2 = new Employee("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009,01,26), Gender.F,56752);
-        Employee e3 = new Employee("Sydney", "Symonds", "ssymonds2@hhs.gov", LocalDate.of(2010,05,17), Gender.F,95313);
-        Employee e4 = new Employee("Avrom", "Rowantree", null, LocalDate.of(2014,03,02), Gender.M,119764);
-        Employee e5 = new Employee("Feliks", "Morffew", "fmorffew4@a8.net", LocalDate.of(2003,01,14), Gender.M,55307);
+        Employee e1 = new Employee("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006,04,20), 154864,Gender.F);
+        Employee e2 = new Employee("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009,01,26),56752,  Gender.F);
+        Employee e3 = new Employee("Sydney", "Symonds", "ssymonds2@hhs.gov", LocalDate.of(2010,05,17), 95313, Gender.F);
+        Employee e4 = new Employee("Avrom", "Rowantree", null, LocalDate.of(2014,03,02),119764, Gender.M);
+        Employee e5 = new Employee("Feliks", "Morffew", "fmorffew4@a8.net", LocalDate.of(2003,01,14), 55307, Gender.M);
 
         Department d1 = new Department("Sports","Outdoors");
         Department d2 = new Department("Tools","Hardware");
