35d2dedbf97442b97c6dd6acdf0039a7568034cc
UmitUmit <umit.green@gmail.com>
2022-04-21 19:37:13 -0400

    OneToOne

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.cydeo.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //I don't want to create any class from this class, I only want to use it as inheritance
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+}
---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/entity/Merchant.java
@@ -0,0 +1,33 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "merchants")
+@Data
+@NoArgsConstructor
+public class Merchant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+    private BigDecimal transactionFee;
+    private BigDecimal commissionRate;
+    private Integer payoutDelayCount;
+
+    @OneToMany(mappedBy = "merchant")
+    private List<Payment> payment;
+
+
+
+
+
+}
