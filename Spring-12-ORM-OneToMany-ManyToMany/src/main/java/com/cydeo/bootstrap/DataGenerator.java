002647bd1a012985ca0b86f5700f59645abf3752
UmitUmit <umit.green@gmail.com>
2022-04-21 20:43:23 -0400

    Mapping

--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,9 +1,11 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.entity.Customer;
 import com.cydeo.entity.Merchant;
 import com.cydeo.entity.Payment;
 import com.cydeo.entity.PaymentDetail;
 import com.cydeo.enums.Status;
+import com.cydeo.repository.CustomerRepository;
 import com.cydeo.repository.MerchantRepository;
 import com.cydeo.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -15,11 +17,13 @@ public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
     private final MerchantRepository merchantRepository;
+    private final CustomerRepository customerRepository;
 
 
-    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository, CustomerRepository customerRepository) {
         this.paymentRepository = paymentRepository;
         this.merchantRepository = merchantRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
@@ -38,7 +42,17 @@ public class DataGenerator implements CommandLineRunner {
 
         Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
 
+        Customer customer1 = new Customer("Smith", "mike", "Smith", "msmsmith@cydeo.com","Virginia");
 
+        payment1.setCustomer(customer1);
+        payment2.setCustomer(customer1);
+
+        customerRepository.save(customer1);
+
+        payment1.setMerchant(merchant1);
+        payment2.setMerchant(merchant1);
+
+        merchantRepository.save(merchant1);
 
     }
 }
